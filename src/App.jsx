c66ac6d54477b250976158260077370efff07b80
Luigi Rauso <darjkzer@hotmail.it>
2023-10-05 18:47:25 +0200

    20-Props: exercise completed

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,3 @@ function App() {
 }
 
 export default App;
-
-/* 
-Q-1: What happens if the name prop is a JSX expression instead of a string?
-A-1: If the name prop is a JSX expression instead of a string, it'll be rendered as a React element.
-
-Q-2: How do you pass a prop that contains a JSX expression?
-A-1: You can pass a prop that contains a JSX expression by including the JSX expression within curly braces { }.
-*/
