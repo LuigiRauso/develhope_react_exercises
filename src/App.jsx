ed288fa40c8cecae198f3e9476095d4fda9d8a14
Luigi Rauso <darjkzer@hotmail.it>
2023-10-05 18:41:52 +0200

    19-Props: exercise completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,17 @@ import Welcome from "./componenets/Welcome";
 function App() {
   return (
     <>
-      <Welcome name="Luigi" age="30" />
+      <Welcome name={<b>Luigi</b>} age="30" />
     </>
   );
 }
 
 export default App;
+
+/* 
+Q-1: What happens if the name prop is a JSX expression instead of a string?
+A-1: If the name prop is a JSX expression instead of a string, it'll be rendered as a React element.
+
+Q-2: How do you pass a prop that contains a JSX expression?
+A-1: You can pass a prop that contains a JSX expression by including the JSX expression within curly braces { }.
+*/
