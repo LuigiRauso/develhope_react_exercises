82caf9fdbaf703f49a8f972051579a52d576b083
Luigi Rauso <darjkzer@hotmail.it>
2023-10-05 12:18:37 +0200

    03-JSX: exercise completed

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-function Greeting({name}) {
+function Greeting() {
   return (
     <h1>Hello, {name}</h1>
   )
 }
 
 export default Greeting
+
+/*
+
+Q: What happens if the name variable is not passed to the function?
+A: If name is not passed to the Greeting function, the component will render with the text "Hello, "
+
+*/
