fc28c8f029f6a28650f7bf86580b2288f6173155
Luigi Rauso <darjkzer@hotmail.it>
2023-10-05 14:44:12 +0200

    07-Components: exercise completed

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Hello from "./componenets/Hello";
+
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(<Hello />);
