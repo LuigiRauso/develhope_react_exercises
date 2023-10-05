b41b7ae3a7a96dba65cd86c089573702d43677b0
Luigi Rauso <darjkzer@hotmail.it>
2023-10-05 18:02:35 +0200

    15-Components: exercise completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,8 @@ function App() {
   return (
     <div>
       <Hello />
-      <Message />
     </div>
   );
 }
 
 export default App;
-
-/* 
-Q: Can you render the Message component directly within the App component? What happens if you do?
-A: Yes, you can. Both Hello and Message components will be displayed. This works because in React, components are reusable and can be rendered within other components.
-*/
