37440b53177490067a6d36015b41559584271eb6
Luigi Rauso <darjkzer@hotmail.it>
2023-10-06 21:57:02 +0200

    38-Handling_Events: exercise completed

--- src/Components/MouseClicker.jsx
+++ src/Components/MouseClicker.jsx
@@ -6,14 +6,23 @@ function MouseClicker(props) {
     console.log(event.currentTarget.name);
   };
 
+  const handleImageClick = (event) => {
+    console.log(event.target.src);
+  };
+
   return (
     <>
       <button name={props.name} onClick={handleClick}>
         Click me!
-        <img src={smile} alt="icon" />
+        <img src={smile} alt="icon" onClick={handleImageClick} />
       </button>
     </>
   );
 }
 
 export default MouseClicker;
+
+/* 
+Q: Does the name attribute of the button still get printed to the console when the image is clicked? Why?
+A: Yes, the name attribute of the button is printed when the image is clicked. This happens due to event bubbling in React, where an event on a child element also triggers on its parent elements.
+*/
