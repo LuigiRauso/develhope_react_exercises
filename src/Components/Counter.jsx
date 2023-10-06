1f27d175a8cb6a5f2086a0220ec25d52d788c6bd
Luigi Rauso <darjkzer@hotmail.it>
2023-10-06 15:23:29 +0200

    30:State: exercise completed

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -16,3 +16,9 @@ function Counter() {
 }
 
 export default Counter;
+
+
+/* 
+Q: When calling "setter" function to increment the counter, should the parameter be a function or an immediate value? Why?
+A: The parameter should be a function. This ensures that the most recent state is used when updating, which is particularly important when multiple state updates are performed simultaneously.
+*/
