80f982dce1670b9ea41d01c9f35cfa87d73ca434
Luigi Rauso <darjkzer@hotmail.it>
2023-10-10 18:23:11 +0200

    60-Styling_Components: exercise completed

--- src/componenets/Welcome.jsx
+++ src/componenets/Welcome.jsx
@@ -3,10 +3,10 @@ import Age from "./Age";
 
 function Welcome({ name = "User", age }) {
   return (
-    <>
+    <div className="welcome">
       <p>Welcome, {name} </p>
       <Age age={age} />
-    </>
+    </div>
   );
 }
 
