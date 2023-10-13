401a205b71d67a7f7143f3717f092b5086b469be
Luigi Rauso <darjkzer@hotmail.it>
2023-10-13 22:49:26 +0200

    13-Components: exercise corrected, fixed typos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import Hello from "./componenets/Hello";
+import Hello from "./components/Hello";
 
 function App() {
   return (
     <div>
+      <Hello />
+      <Hello />
       <Hello />
     </div>
   );
 }
 
 export default App;
+
+// Scusa, ci sono stati problemi durante il push. Grazie per avermi fatto notare l'errore, allego di nuovo la risposta:
+
+/* 
+Q: Can you use the Hello component more than once in the App component? What happens if you do?
+A: Yes, you can use the Hello component multiple times in the App component. Each instance will be rendered independently on the page.
+*/
--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,3 @@ import App from "./App";
 
 const root = document.getElementById("root");
 ReactDOM.createRoot(root).render(<App />);
-
-
-// The application was successfully initiated using the 'npm run dev' command, and it's functioning flawlessly as anticipated.
