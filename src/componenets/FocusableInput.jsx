432cb327a06867b5b7527a6e781b542e0e9abb21
Luigi Rauso <darjkzer@hotmail.it>
2023-10-17 10:32:25 +0200

    66-Styling_Components: exercise completed

--- src/componenets/FocusableInput.jsx
+++ src/componenets/FocusableInput.jsx
@@ -8,10 +8,18 @@ function FocusableInput() {
   }, []);
 
   return (
-    <>
-      <label htmlFor="username">Username: </label>
-      <input ref={inputRef} type="text" name="username" id="username" />
-    </>
+    <div className="mx-auto card" style={{maxWidth: "320px", marginTop: "1rem"}}>
+      <div className="card-body">
+        <h5 className="text-center">Login</h5>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="username" className="form-label">Username</label>
+            <input ref={inputRef} type="text" name="username" id="username" className="form-control" />
+          </div>
+          <button type="submit" className="btn btn-primary w-100">Submit</button>
+        </form>
+      </div>
+    </div>
   );
 }
 
