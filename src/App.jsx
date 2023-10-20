57c4466829d28974089a6b4136cc3074f675c742
Luigi Rauso <darjkzer@hotmail.it>
2023-10-20 12:09:29 +0200

    70-Context: exercise completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
 import Clock from "./Components/Clock";
+import LanguageContext from "./Components/LanguageContext";
 
 function App() {
+  const [language, setLanguage] = useState("en");
+
+  const handleLanguageChange = (event) => {
+    setLanguage(event.target.value);
+  };
   return (
     <>
-      <Clock />
+      <LanguageContext.Provider value={language}>
+        <div>
+          <Clock />
+          <select
+            name="language"
+            id="language"
+            value={language}
+            onChange={handleLanguageChange}
+          >
+            <option value="it">Italian</option>
+            <option value="en">English</option>
+            <option value="fr">French</option>
+            <option value="es">Spanish</option>
+            <option value="sr">Serbian</option>
+          </select>
+        </div>
+      </LanguageContext.Provider>
     </>
   );
 }
