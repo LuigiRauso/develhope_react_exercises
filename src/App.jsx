2517e02d918e2c1adbffeb660b99608fee5b742f
Luigi Rauso <darjkzer@hotmail.it>
2023-10-24 21:52:52 +0200

    85-React_Router: exercise completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Welcome from "./components/Welcome";
 import Counter from "./Components/Counter";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import ShowGithubUser from "./components/ShowGithubUser";
 
 function App() {
   return (
     <>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/counter">Counter</Link>
+        <Link to="/users/luigirauso">My Github Page</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Welcome name="John" age="17" />} />
         <Route
